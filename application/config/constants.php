fc743eb560506a1832eddedd81e4d5eea2fe6cee
wangnan <wangnan@anhao.com>
2016-11-17 18:32:42 +0830

    增加一些常量

--- application/config/constants.php
+++ application/config/constants.php
@@ -87,9 +87,11 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 /* COMMENT CONSTANT */
 defined('DS') OR define('DS', '/');  // DIRECTORY_SEPARATOR 目录分隔符简化写法
 /* PATH */
-defined('PATH_ROOT') OR define('PATH_ROOT', dirname(dirname(__DIR__)) . DS);       // 项目跟路径
-defined('PATH_APP') OR define('PATH_APP', PATH_ROOT . 'application' . DS);         // APP项目路径
-defined('PATH_SESS_FILE') OR define('PATH_SESS_FILE', PATH_APP . 'cache/session');  // session保存路径
+defined('PATH_ROOT')  OR define('PATH_ROOT', dirname(dirname(__DIR__)) . DS);    // 项目跟路径
+defined('PATH_APP')   OR define('PATH_APP', PATH_ROOT . 'application' . DS);     // APP项目路径
+defined('PATH_CACHE') OR define('PATH_CACHE', PATH_APP . 'cache' . DS);          // Catch目录
+defined('PATH_SESS')  OR define('PATH_SESS', PATH_CACHE . 'session' . DS);       // session保存路径
+defined('PATH_MODEL') OR define('PATH_MODEL', PATH_APP . 'models' . DS);         // model存放路径
 
 /* Admin CONSTANT */
 defined('ADMIN_PAGE_SIZE') OR define('ADMIN_PAGE_SIZE', 20); // 分页大小
