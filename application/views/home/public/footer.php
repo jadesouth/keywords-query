e894f3ba6c1a486462289e05907546af2bd1a3a8
wangnan <wangnan@anhao.com>
2016-11-17 18:35:36 +0830

    增加前台header和footer

---
+++ application/views/home/public/footer.php
@@ -0,0 +1,45 @@
+</div>
+<div class="footer clearfix">
+    <div class="m-width clearfix">
+        <div class="fr clearfix">
+            <ul class="clearfix">
+                <li><a href="">关于我们</a></li>
+                <li><a href="">新闻发布</a></li>
+                <li><a href="">招聘</a></li>
+                <li><a href="">隐私条款</a></li>
+                <li><a href="">常见问题</a></li>
+                <li><a href="">联系我们</a></li>
+            </ul>
+            <p class="info">@2015 Anhaotimes Inc All Rights Reserved<span>京ICP备16005139号-1</span></p>
+            <p class="phone">联系客服： 400-9100-665</p>
+        </div>
+    </div>
+</div>
+
+<!-- 弹出层部分begin -->
+<div class="denglu">
+    <div class="content">
+        <div class="m-width clearfix">
+            <div class="dengl clearfix">
+                <form>
+                    <dl class="clearfix">
+                        <dt class="close"><span>关闭</span></dt>
+                        <dt class="phone"><p><i></i><input type="text" placeholder="请输入您的手机号" value="" /></p></dt>
+                        <dt class="img-code"><p><i></i><input type="text" placeholder="请输入左侧的图形" value="" /></p></dt>
+                        <dt class="phone-code"><p><i></i><input type="text" placeholder="请输入手机收到的验证码" value="" /></p></dt>
+                        <dd class="ck clearfix"><input type="checkbox" value="1" />记住我（如在公共区域，请不要点选此项）</dd>
+                        <dd class="su clearfix"><input type="button" value="登&nbsp;&nbsp;录" /></dd>
+                        <dd>* 如果您并未注册，登录后系统将自动为您创建安好账号</dd>
+                    </dl>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content_mark"></div>
+<!-- 弹出层部分end -->
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="<?=base_url()?>resources/assets/libs/modernizr-custom/modernizr-custom-v2.7.1.min.js"></script>
+<script src="<?=base_url()?>resources/assets/js/home/style.js"></script>
+</body>
+</html>
---
+++ application/views/home/public/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>首页</title>
+    <link href="<?=base_url()?>resources/assets/css/home/style.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="header clearfix">
+    <div class="m-width clearfix">
+        <div class="h-logo fl"><i class="fl"><a href=""><img src="/resources/assets/images/logo.png"/></a></i>安好专家</div>
+        <div class="h-hot-menu fl"><a href="" class="menu-hover">看专家</a><a href="">安好客户端</a><a href="">家庭医生</a><a href="">联系我们</a></div>
+        <div class="fr login-btn">登陆</div><!--<div class="fr login-info">欢迎您：<a href="">某某某某某某某某</a></div>-->
+    </div>
+</div>
+<div class="m-width">
