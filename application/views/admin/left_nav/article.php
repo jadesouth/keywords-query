db6230da6e2eaea3a5f10854a409e4c33afe568f
wangnanphp <wangnanphp@163.com>
2016-11-21 00:36:58 +0800

    代码整理

--- application/views/admin/left_nav/article.php
+++ application/views/admin/left_nav/article.php
@@ -4,8 +4,8 @@
         <!-- left nav -->
         <div class="col-sm-3 col-md-2 sidebar">
             <ul class="nav nav-sidebar">
-                <li <?='index' == $method_name ? 'class="active"' : ''?>><a href="<?=base_url('admin/article/index')?>">文章列表 <?='index' == $method_name ? '<span class="sr-only">(current)</span>' : ''?></a></li>
-                <li <?='add' == $method_name ? 'class="active"' : ''?>><a href="<?=base_url('admin/article/add')?>">添加文章 <?='add' == $method_name ? '<span class="sr-only">(current)</span>' : ''?></a></li>
+                <li <?='index' == $method_name ? 'class="active"' : ''?>><a href="<?=base_url('admin/article/index')?>">文章列表<?='index' == $method_name ? '<span class="sr-only">(current)</span>' : ''?></a></li>
+                <li <?='add' == $method_name ? 'class="active"' : ''?>><a href="<?=base_url('admin/article/add')?>">添加文章<?='add' == $method_name ? '<span class="sr-only">(current)</span>' : ''?></a></li>
             </ul>
         </div>
         <!-- right content -->
--- application/views/admin/left_nav/user.php
+++ application/views/admin/left_nav/user.php
@@ -4,9 +4,9 @@
         <!-- left nav -->
         <div class="col-sm-3 col-md-2 sidebar">
             <ul class="nav nav-sidebar">
-                <li <?='all' == $user_type ? 'class="active"' : ''?>><a href="<?=base_url('/admin/user/?type=all')?>">用户列表 <?='all' == $user_type ? '<span class="sr-only">(current)</span>' : ''?></a></li>
-                <li <?='authorized' == $user_type ? 'class="active"' : ''?>><a href="<?=base_url('/admin/user/?type=authorized')?>">有权限的用户列表 <?='authorized' == $user_type ? '<span class="sr-only">(current)</span>' : ''?></a></li>
-                <li <?='normal' == $user_type ? 'class="active"' : ''?>><a href="<?=base_url('/admin/user/?type=normal')?>">无权限的用户列表 <?='normal' == $user_type ? '<span class="sr-only">(current)</span>' : ''?></a></li>
+                <li <?='all' == $user_type ? 'class="active"' : ''?>><a href="<?=base_url('/admin/user/?type=all')?>">用户列表<?='all' == $user_type ? '<span class="sr-only">(current)</span>' : ''?></a></li>
+                <li <?='authorized' == $user_type ? 'class="active"' : ''?>><a href="<?=base_url('/admin/user/?type=authorized')?>">有权限的用户列表<?='authorized' == $user_type ? '<span class="sr-only">(current)</span>' : ''?></a></li>
+                <li <?='normal' == $user_type ? 'class="active"' : ''?>><a href="<?=base_url('/admin/user/?type=normal')?>">无权限的用户列表<?='normal' == $user_type ? '<span class="sr-only">(current)</span>' : ''?></a></li>
             </ul>
         </div>
         <!-- right content -->
