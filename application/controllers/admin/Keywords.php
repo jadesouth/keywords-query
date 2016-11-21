70182f17ea1771cdd3a356a555489bbd8dfb3fda
wangnanphp <wangnanphp@163.com>
2016-11-21 22:27:48 +0800

    debug

--- application/controllers/admin/Keywords.php
+++ application/controllers/admin/Keywords.php
@@ -72,24 +72,56 @@ class Keywords extends Admin_Controller
      */
     public function add()
     {
-        $this->load->helper('http');
-        $this->load->library('form_validation');
-        if (false === $this->form_validation->run()) {
-            http_ajax_response(1, $this->form_validation->error_string());
-            return false;
-        }
+        if('post' == $this->input->method()) {
+            $this->load->helper('http');
+            $this->load->library('form_validation');
+            if (false === $this->form_validation->run()) {
+                http_ajax_response(1, $this->form_validation->error_string());
+                return false;
+            }
 
-        $type = $this->input->post('type');
-        $word = $this->input->post('word');
-        $res = $this->_model
-            ->setInsertData(['type' => $type, 'word' => $word])
-            ->create();
-        if (0 >= $res) {
-            http_ajax_response(2, '添加关键字失败,请稍后再试!');
-            return false;
-        }
+            $type = $this->input->post('type');
+            $word = $this->input->post('word');
+            $res = $this->_model
+                ->setInsertData(['type' => $type, 'word' => $word])
+                ->create();
+            if (0 >= $res) {
+                http_ajax_response(2, '添加关键字失败,请稍后再试!');
+                return false;
+            }
 
-        http_ajax_response(0, '添加关键字成功!', ['id' => $res, 'word' => $word]);
-        return true;
+            http_ajax_response(0, '添加关键字成功!', ['id' => $res, 'word' => $word]);
+            return true;
+        } else {
+            $this->advertising();
+            return true;
+        }
+    }
+
+    /**
+     * index 列表方法定向到 advertising
+     *
+     * @param int $page 数据表主键
+     *
+     * @author wangnan <wangnanphp@163.com>
+     * @date 2016-11-21 22:13:27
+     */
+    public function index(int $page = 0)
+    {
+        $this->advertising();
+    }
+
+    /**
+     * edit 编辑方法定向到 advertising
+     *
+     * @param int $id 数据表主键
+     * @return json|void
+     *
+     * @author wangnan <wangnanphp@163.com>
+     * @date   2016-11-21 22:12:42
+     */
+    public function edit(int $id = 0)
+    {
+        $this->advertising();
     }
 }
