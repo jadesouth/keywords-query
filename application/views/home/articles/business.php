efbfa051bb3af06ab6dd5375b3bb4fd65d06802f
wangnanphp <wangnanphp@163.com>
2017-01-08 21:12:13 +0800

    debug

--- application/views/home/articles/business.php
+++ application/views/home/articles/business.php
@@ -1,5 +1,5 @@
-<div class="m-width" style="margin-left:35px;">
-    <div style="margin-top:20px; padding:0 60px;">
+<div class="m-width" style="margin:35px auto;">
+    <div style="margin-top:20px; padding:0 30px;">
         <?=$data['content']?>
     </div>
 </div>
